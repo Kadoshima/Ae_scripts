2a4999fcb33e5c77eb908668ea2b46d2b486ecec
kadoshima <galaxykadosima@gmail.com>
2022-02-16 01:43:10 +0900

    first commits

---
+++ GUItest.jsx
@@ -0,0 +1,35 @@
+﻿#include "autoslide.jsx"
+
+//　ダイアログ作成
+var  objDlg = new Window ("dialog", "タブ", [0,0,200,250]);
+
+//　タブパネルを追加
+var objTbPnl = objDlg.add("tabbedpanel", [20,20,180,230]);
+
+//タブパネルにタブを2つ追加
+var objTb01 = objTbPnl.add("tab", undefined, "スライド");
+var objTb02 = objTbPnl.add("tab", undefined, "ツリー");
+
+//各タブにアイテムを挿入
+var SldAll = objTb01.add("button", [20, 0, 80, 120], "一括変更");
+var Imgchg = objTb01.add("button", [90, 0, 150, 50], "画像のみ");
+var Textchg = objTb01.add("button", [90, 70, 150, 120], "テキストのみ");
+
+//　ダイアログを画面センターに表示
+objDlg.center();
+objDlg.show();
+
+SldAll.onClick = function(){
+    insert();
+    SizeControl();
+    textcontrol();
+}
+
+Imgchg.onClick = function(){
+    insert();
+    SizeControl();
+}
+
+Textchg.onClick = function(){
+    textcontrol();
+}
---
+++ fileopen.jsx
@@ -0,0 +1,20 @@
+﻿var fileobj=File.openDialog("open", ["text:*.txt"]);
+
+if(fileobj.open("r")){
+    var text = fileobj.read();
+    fileobj.close();
+
+    var atext = ['null'];
+    
+    var count = 1;
+    for(var i = 0; i < text.length; i++){
+        if(text[i] == '\n' || text[i] =='\r' || text[i] =='\r\n'){
+            count++;
+            atext.push("");
+            }
+        else{
+            atext[count] += text[i];
+            }
+        }
+    return atext;
+    }
