d6fdfcfbd92259b19d161a023d8f80255e1a64db
Kadoshima <88878835+Kadoshima@users.noreply.github.com>
2022-06-17 13:01:47 +0900

    Delete ソース 2.jsx

--- ソース 2.jsx
+++
@@ -1,7 +0,0 @@
-﻿// ボタン を 追加 する。 
-var myWin = new Window("palette", "title"); 
-var btn = myWin. add("button", undefined, "Hello"); 
-btn. onClick = function(){ 
-    alert(" Hello world!"); 
-    } 
-myWin. show();
